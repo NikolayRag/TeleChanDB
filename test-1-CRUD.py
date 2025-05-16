71f8ca781680efc7e9397799c389d4dade43c316
NikolayRag <thenickra@gmail.com>
2025-05-16 05:11:45 +0300

    clean

--- test-1-CRUD.py
+++ test-1-CRUD.py
@@ -46,8 +46,6 @@ testId = tcdb.write('By id.', tags={'someTag':2})
 tagsV = tcdb.list(['someTag', ''], ids=True)
 print(f"Tags listed: {tagsV}")
 
-tagsV = tcdb.list(['someTag', 'test', ''])
-print(f"Tags values: {tagsV}")
 readData = tcdb.read(tags={'someTag':1}, ids=testId)
 for cId,cRecord in readData.items():
 	print(f"{cId}: {json.dumps(cRecord, indent=4)}")
