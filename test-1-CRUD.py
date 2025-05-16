155065b95fa965da757fe507a3967dbf8b07401b
NikolayRag <thenickra@gmail.com>
2025-05-16 05:11:31 +0300

    add test read case

--- test-1-CRUD.py
+++ test-1-CRUD.py
@@ -42,8 +42,12 @@ print(f"Test Create")
 tcdb.write('The first test record.', tags={'test':None, 'someTag':1})
 tcdb.write('Second record.', tags={'test':None, 'someTag':2})
 tcdb.write('One more record.', tags={'someTag':3, 'otherTag':None})
+testId = tcdb.write('By id.', tags={'someTag':2})
 tagsV = tcdb.list(['someTag', ''], ids=True)
 print(f"Tags listed: {tagsV}")
 
 tagsV = tcdb.list(['someTag', 'test', ''])
 print(f"Tags values: {tagsV}")
+readData = tcdb.read(tags={'someTag':1}, ids=testId)
+for cId,cRecord in readData.items():
+	print(f"{cId}: {json.dumps(cRecord, indent=4)}")
